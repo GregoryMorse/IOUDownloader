5e4a7a23375dd2e4e4fda148dd97f7f5a87dc98e
GregoryMorse <gregory.morse@live.com>
2015-07-31 05:09:31 +0200

    Initial file checkin

---
+++ frmIOUDown.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmIOUDown
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.rbDiploma = New System.Windows.Forms.RadioButton()
+        Me.rbMainCampus = New System.Windows.Forms.RadioButton()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.lbCourseList = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblError = New System.Windows.Forms.Label()
+        Me.btnDownload = New System.Windows.Forms.Button()
+        Me.lbFiles = New System.Windows.Forms.ListBox()
+        Me.fbdMain = New System.Windows.Forms.FolderBrowserDialog()
+        Me.btnSetDownloadFolder = New System.Windows.Forms.Button()
+        Me.txtDownloadFolder = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'rbDiploma
+        '
+        Me.rbDiploma.AutoSize = True
+        Me.rbDiploma.Checked = True
+        Me.rbDiploma.Location = New System.Drawing.Point(12, 12)
+        Me.rbDiploma.Name = "rbDiploma"
+        Me.rbDiploma.Size = New System.Drawing.Size(63, 17)
+        Me.rbDiploma.TabIndex = 0
+        Me.rbDiploma.TabStop = True
+        Me.rbDiploma.Text = "Diploma"
+        Me.rbDiploma.UseVisualStyleBackColor = True
+        '
+        'rbMainCampus
+        '
+        Me.rbMainCampus.AutoSize = True
+        Me.rbMainCampus.Location = New System.Drawing.Point(81, 12)
+        Me.rbMainCampus.Name = "rbMainCampus"
+        Me.rbMainCampus.Size = New System.Drawing.Size(89, 17)
+        Me.rbMainCampus.TabIndex = 1
+        Me.rbMainCampus.TabStop = True
+        Me.rbMainCampus.Text = "Main Campus"
+        Me.rbMainCampus.UseVisualStyleBackColor = True
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(278, 12)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(133, 20)
+        Me.txtUsername.TabIndex = 2
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(278, 38)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(133, 20)
+        Me.txtPassword.TabIndex = 3
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(424, 15)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(90, 43)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'lbCourseList
+        '
+        Me.lbCourseList.FormattingEnabled = True
+        Me.lbCourseList.Location = New System.Drawing.Point(12, 84)
+        Me.lbCourseList.Name = "lbCourseList"
+        Me.lbCourseList.Size = New System.Drawing.Size(406, 69)
+        Me.lbCourseList.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(217, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(217, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Password"
+        '
+        'lblError
+        '
+        Me.lblError.AutoSize = True
+        Me.lblError.ForeColor = System.Drawing.Color.Red
+        Me.lblError.Location = New System.Drawing.Point(142, 65)
+        Me.lblError.Name = "lblError"
+        Me.lblError.Size = New System.Drawing.Size(0, 13)
+        Me.lblError.TabIndex = 8
+        '
+        'btnDownload
+        '
+        Me.btnDownload.Location = New System.Drawing.Point(424, 95)
+        Me.btnDownload.Name = "btnDownload"
+        Me.btnDownload.Size = New System.Drawing.Size(90, 43)
+        Me.btnDownload.TabIndex = 9
+        Me.btnDownload.Text = "Download"
+        Me.btnDownload.UseVisualStyleBackColor = True
+        '
+        'lbFiles
+        '
+        Me.lbFiles.FormattingEnabled = True
+        Me.lbFiles.Location = New System.Drawing.Point(12, 184)
+        Me.lbFiles.Name = "lbFiles"
+        Me.lbFiles.Size = New System.Drawing.Size(502, 69)
+        Me.lbFiles.TabIndex = 10
+        '
+        'btnSetDownloadFolder
+        '
+        Me.btnSetDownloadFolder.Location = New System.Drawing.Point(424, 148)
+        Me.btnSetDownloadFolder.Name = "btnSetDownloadFolder"
+        Me.btnSetDownloadFolder.Size = New System.Drawing.Size(90, 34)
+        Me.btnSetDownloadFolder.TabIndex = 11
+        Me.btnSetDownloadFolder.Text = "Set Download Folder"
+        Me.btnSetDownloadFolder.UseVisualStyleBackColor = True
+        '
+        'txtDownloadFolder
+        '
+        Me.txtDownloadFolder.Location = New System.Drawing.Point(12, 156)
+        Me.txtDownloadFolder.Name = "txtDownloadFolder"
+        Me.txtDownloadFolder.Size = New System.Drawing.Size(406, 20)
+        Me.txtDownloadFolder.TabIndex = 12
+        '
+        'frmIOUDown
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(526, 323)
+        Me.Controls.Add(Me.txtDownloadFolder)
+        Me.Controls.Add(Me.btnSetDownloadFolder)
+        Me.Controls.Add(Me.lbFiles)
+        Me.Controls.Add(Me.btnDownload)
+        Me.Controls.Add(Me.lblError)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbCourseList)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.rbMainCampus)
+        Me.Controls.Add(Me.rbDiploma)
+        Me.Name = "frmIOUDown"
+        Me.Text = "IOU Downloader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents rbDiploma As System.Windows.Forms.RadioButton
+    Friend WithEvents rbMainCampus As System.Windows.Forms.RadioButton
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents lbCourseList As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblError As System.Windows.Forms.Label
+    Friend WithEvents btnDownload As System.Windows.Forms.Button
+    Friend WithEvents lbFiles As System.Windows.Forms.ListBox
+    Friend WithEvents fbdMain As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents btnSetDownloadFolder As System.Windows.Forms.Button
+    Friend WithEvents txtDownloadFolder As System.Windows.Forms.TextBox
+End Class
---
+++ frmIOUDown.vb
@@ -0,0 +1,184 @@
+﻿Public Class frmIOUDown
+
+    Public IOUCampus As String = "http://www.islamiconlineuniversity.com/campus"
+    Public IOUOpenCampus As String = "http://www.islamiconlineuniversity.com/opencampus"
+    Public Extensions As String = "*.pdf,*.pptx,*.ppt,*.docx,*.doc,*.rtf,*.xlsx,*.xls,*.mp3,*.mp4,*.flv"
+    Public CourseDownloadFolder As String
+    Public Token As String
+    Public UserID As String
+    Public LoginCookies As Net.CookieCollection
+    Structure CourseItem
+        Public ID As String
+        Public ShortName As String
+        Public FullName As String
+        Public Overrides Function ToString() As String
+            Return FullName
+        End Function
+    End Structure
+    Structure FileItem
+        Public FileURL As String
+        Public FileName As String
+        Public TimeModified As String
+        Public Overrides Function ToString() As String
+            Return FileName
+        End Function
+    End Structure
+    Public Sub PopulateCourseList()
+        lbCourseList.Items.Clear()
+        Dim Req As Net.HttpWebRequest = Net.WebRequest.Create(If(rbDiploma.Checked, IOUOpenCampus, IOUCampus) + "/webservice/rest/server.php?wstoken=" + Token + "&wsfunction=core_enrol_get_users_courses&userid=" + UserID)
+        Dim Resp As Net.HttpWebResponse = Req.GetResponse()
+        Dim MemStream As New IO.MemoryStream
+        Resp.GetResponseStream().CopyTo(MemStream)
+        MemStream.Seek(0, IO.SeekOrigin.Begin)
+        Dim Reader As System.Xml.XmlReader
+        If Resp.ContentType = "application/xml; charset=utf-8" Then
+            Reader = Xml.XmlReader.Create(MemStream)
+        Else 'application/json; charset=utf-8
+            Reader = System.Runtime.Serialization.Json.JsonReaderWriterFactory.CreateJsonReader(MemStream.ToArray(), New System.Xml.XmlDictionaryReaderQuotas())
+        End If
+        Reader.Read()
+        Dim XmlDoc As New Xml.XmlDocument
+        XmlDoc.Load(Reader)
+        Dim CourseNodes As Xml.XmlNodeList = XmlDoc.SelectNodes("/RESPONSE/MULTIPLE/SINGLE")
+        For Count = 0 To CourseNodes.Count - 1
+            lbCourseList.Items.Add(New CourseItem With {.FullName = CourseNodes(Count).SelectSingleNode("KEY[@name='fullname']/VALUE").InnerText, .ShortName = CourseNodes(Count).SelectSingleNode("KEY[@name='shortname']/VALUE").InnerText, .ID = CourseNodes(Count).SelectSingleNode("KEY[@name='id']/VALUE").InnerText})
+        Next
+        Reader.Close()
+        Resp.Close()
+    End Sub
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        lblError.Text = "Logging in..."
+        'moodle_mobile_app does not have REST access only XML RPC
+        'local_mobile service - additional features not tested yet
+        Dim Req As Net.HttpWebRequest = Net.WebRequest.Create(If(rbDiploma.Checked, IOUOpenCampus, IOUCampus) + "/login/token.php?username=" + Net.WebUtility.HtmlEncode(txtUsername.Text) + "&password=" + Net.WebUtility.HtmlEncode(txtPassword.Text) + "&service=android")
+        Dim Resp As Net.HttpWebResponse = Req.GetResponse()
+        Dim MemStream As New IO.MemoryStream
+        Resp.GetResponseStream().CopyTo(MemStream)
+        MemStream.Seek(0, IO.SeekOrigin.Begin)
+        Dim Reader As System.Xml.XmlReader
+        If Resp.ContentType = "application/xml; charset=utf-8" Then
+            Reader = Xml.XmlReader.Create(MemStream)
+        Else 'application/json; charset=utf-8
+            Reader = System.Runtime.Serialization.Json.JsonReaderWriterFactory.CreateJsonReader(MemStream.ToArray(), System.Xml.XmlDictionaryReaderQuotas.Max)
+        End If
+        Dim XmlDoc As New Xml.XmlDocument
+        XmlDoc.Load(Reader)
+        If Not XmlDoc.SelectSingleNode("/root/error") Is Nothing Then
+            lblError.Text = XmlDoc.SelectSingleNode("/root/error").InnerText
+        Else
+            UserID = XmlDoc.SelectSingleNode("/root/userid").InnerText
+            Token = XmlDoc.SelectSingleNode("/root/token").InnerText
+            lblError.Text = String.Empty
+            PopulateCourseList()
+        End If
+        Reader.Close()
+        Resp.Close()
+    End Sub
+    Public Sub GetLoginCookies()
+        Dim Req As Net.HttpWebRequest = Net.WebRequest.Create("http://www.islamiconlineuniversity.com/" + If(rbDiploma.Checked, "opencampus", "campus") + "/login/index.php")
+        Req.AllowAutoRedirect = False
+        Dim PostData As String = "username=" + Net.WebUtility.HtmlEncode(txtUsername.Text) + "&password=" + Net.WebUtility.HtmlEncode(txtPassword.Text) + "&rememberusername=1"
+        Dim Buf As Byte() = System.Text.Encoding.UTF8.GetBytes(PostData)
+        Req.Method = "POST"
+        Req.ContentLength = Buf.Length
+        Req.ContentType = "application/x-www-form-urlencoded"
+        Dim ReqStream As IO.Stream = Req.GetRequestStream()
+        ReqStream.Write(Buf, 0, Buf.Length)
+        ReqStream.Close()
+        Dim Resp As Net.HttpWebResponse = Req.GetResponse()
+        Dim Stream As New IO.StreamReader(Resp.GetResponseStream())
+        Dim Str As String = Stream.ReadToEnd()
+        If Resp.Cookies.Count = 0 Then
+            LoginCookies = New Net.CookieCollection
+            For Count As Integer = 0 To Resp.Headers.Count - 1
+                If Resp.Headers.GetKey(Count) = "Set-Cookie" Then
+                    Dim Matches As System.Text.RegularExpressions.MatchCollection = System.Text.RegularExpressions.Regex.Matches(Resp.Headers.Get(Count), "(.+?)=(.+?);(?: expires=.*?;)?(?: path=(.+?))?.*?(,|$)")
+                    For MatchCount = 0 To Matches.Count - 1
+                        LoginCookies.Add(New Net.Cookie(Matches(MatchCount).Groups(1).Value, Matches(MatchCount).Groups(2).Value, Matches(MatchCount).Groups(3).Value, Resp.ResponseUri.Host))
+                    Next
+                End If
+            Next
+        Else
+            LoginCookies = Resp.Cookies
+        End If
+        Stream.Close()
+        Resp.Close()
+    End Sub
+    Public Sub CrawlUrl(Url As String, Name As String)
+        Dim Req As Net.HttpWebRequest = Net.WebRequest.Create(Url)
+        Req.CookieContainer = New Net.CookieContainer
+        Req.CookieContainer.Add(LoginCookies)
+        Dim Resp As Net.HttpWebResponse = Req.GetResponse()
+        Dim Stream As New IO.StreamReader(Resp.GetResponseStream())
+        Dim Str As String = Stream.ReadToEnd()
+        If System.Text.RegularExpressions.Regex.Match(Str, "https:\/\/www\.wiziq\.com\/class\/download.aspx\?.*(?=\"")").Success Then
+            lbFiles.Items.Add(New FileItem With {.FileName = Name.Replace(" ", String.Empty) + ".exe", .FileURL = System.Text.RegularExpressions.Regex.Match(Str, "https:\/\/www\.wiziq\.com\/class\/download.aspx\?.*(?=\"")").Value})
+        ElseIf System.Text.RegularExpressions.Regex.Match(Str, "class=\""next\"" href=\""(.*)\""").Success Then
+            CrawlUrl(New Uri(New Uri(Url).GetLeftPart(UriPartial.Path) + "\..\").GetLeftPart(UriPartial.Path) + Net.WebUtility.HtmlDecode(System.Text.RegularExpressions.Regex.Match(Str, "class=\""next\"" href=\""(.*)\""").Groups(1).Value), String.Empty)
+        End If
+        Dim Matches As System.Text.RegularExpressions.MatchCollection = System.Text.RegularExpressions.Regex.Matches(Str, "http:\/\/www.islamiconlineuniversity.com\/(?:open)?campus/pluginfile\.php.*(?=\"".*\>(.*)\<\/a\>)")
+        For MatchCount = 0 To Matches.Count - 1
+            lbFiles.Items.Add(New FileItem With {.FileName = Matches(MatchCount).Groups(1).Value, .FileURL = Matches(MatchCount).Value})
+        Next
+    End Sub
+    Public Sub AddFileNodes(CourseNodes As Xml.XmlNodeList)
+        For Count = 0 To CourseNodes.Count - 1
+            AddFileNodes(CourseNodes(Count).SelectNodes("KEY/MULTIPLE/SINGLE"))
+            If Not CourseNodes(Count).SelectSingleNode("KEY[@name='type']/VALUE") Is Nothing AndAlso CourseNodes(Count).SelectSingleNode("KEY[@name='type']/VALUE").InnerText = "file" Then
+                If Not CourseNodes(Count).SelectSingleNode("KEY[@name='filename']/VALUE").InnerText.EndsWith(".html") Then
+                    lbFiles.Items.Add(New FileItem With {.FileName = CourseNodes(Count).SelectSingleNode("KEY[@name='filename']/VALUE").InnerText, .FileURL = CourseNodes(Count).SelectSingleNode("KEY[@name='fileurl']/VALUE").InnerText})
+                End If
+            ElseIf Not CourseNodes(Count).SelectSingleNode("KEY[@name='modname']/VALUE") Is Nothing AndAlso (CourseNodes(Count).SelectSingleNode("KEY[@name='modname']/VALUE").InnerText = "data" Or CourseNodes(Count).SelectSingleNode("KEY[@name='modname']/VALUE").InnerText = "wiziq") Then
+                CrawlUrl(CourseNodes(Count).SelectSingleNode("KEY[@name='url']/VALUE").InnerText, CourseNodes(Count).SelectSingleNode("KEY[@name='name']/VALUE").InnerText)
+            End If
+        Next
+    End Sub
+    Private Sub btnDownload_Click(sender As Object, e As EventArgs) Handles btnDownload.Click
+        GetLoginCookies()
+        'how to get a sesskey?
+        '"http://www.islamiconlineuniversity.com/opencampus/mod/wiziq/index.php?id=" + CourseID + "&sesskey=" + "&download=xhtml"
+        Dim Req As Net.HttpWebRequest = Net.WebRequest.Create(If(rbDiploma.Checked, IOUOpenCampus, IOUCampus) + "/webservice/rest/server.php?wstoken=" + Token + "&wsfunction=core_course_get_contents&courseid=" + lbCourseList.SelectedItem.ID())
+        lbFiles.Items.Clear()
+        Dim Resp As Net.HttpWebResponse = Req.GetResponse()
+        Dim MemStream As New IO.MemoryStream
+        Resp.GetResponseStream().CopyTo(MemStream)
+        MemStream.Seek(0, IO.SeekOrigin.Begin)
+        Dim Reader As System.Xml.XmlReader
+        If Resp.ContentType = "application/xml; charset=utf-8" Then
+            Reader = Xml.XmlReader.Create(MemStream)
+        Else 'application/json; charset=utf-8
+            Reader = System.Runtime.Serialization.Json.JsonReaderWriterFactory.CreateJsonReader(MemStream.ToArray(), New System.Xml.XmlDictionaryReaderQuotas())
+        End If
+        Dim XmlDoc As New Xml.XmlDocument
+        XmlDoc.Load(Reader)
+        AddFileNodes(XmlDoc.SelectNodes("/RESPONSE/MULTIPLE/SINGLE"))
+        Reader.Close()
+        Resp.Close()
+        Dim Path As String = If(txtDownloadFolder.Text = String.Empty, String.Empty, txtDownloadFolder.Text + "\") + CStr(lbCourseList.SelectedItem.ShortName).Replace(" ", String.Empty)
+        If Not IO.Directory.Exists(Path) Then
+            IO.Directory.CreateDirectory(Path)
+        End If
+        For Count As Integer = 0 To lbFiles.Items.Count - 1
+            Dim FileReq As Net.HttpWebRequest = Net.WebRequest.Create(lbFiles.Items(Count).FileURL)
+            Dim FileResp As Net.WebResponse = FileReq.GetResponse()
+            Dim RespStream As IO.Stream = FileResp.GetResponseStream()
+            'check modified/creation date
+            Dim FStream As IO.FileStream = IO.File.OpenWrite(Path + "\" + lbFiles.Items(Count).FileName)
+            Dim Buf(4095) As Byte
+            Dim BytesRead As Integer = RespStream.Read(Buf, 0, 4096)
+            While BytesRead > 0
+                FStream.Write(Buf, 0, BytesRead)
+                BytesRead = RespStream.Read(Buf, 0, 4096)
+            End While
+            RespStream.Close()
+            FStream.Close()
+            FileResp.Close()
+        Next
+    End Sub
+
+    Private Sub btnSetDownloadFolder_Click(sender As Object, e As EventArgs) Handles btnSetDownloadFolder.Click
+        If fbdMain.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            txtDownloadFolder.Text = fbdMain.SelectedPath
+        End If
+    End Sub
+End Class
